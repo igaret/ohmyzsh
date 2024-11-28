17e1f78473eefe994f8cdfab95ac49c2307a55f4
igaret <148014802+igaret@users.noreply.github.com>
2024-11-28 14:03:26 -0800

    Update install_rish.sh

--- install_rish.sh
+++ install_rish.sh
@@ -1,8 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/sh
-
 cd $TMPDIR
 git clone https://github.com/igaret/termux_stuff
 cp -r $TMPDIR/termux_stuff/termux_rish/* $PREFIX/
-chmod a+x $PREFIX/bin/rish
+chmod a+x $PREFIX/bin/*
 cd ~
 rm -rf $TNPDIR/termux_stuff
