ac35b49b3258af018ef5c997474667e7c02945b2
igaret <148014802+igaret@users.noreply.github.com>
2025-01-03 03:40:03 -0800

    Delete install_rish.sh

--- install_rish.sh
+++
@@ -1,7 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/sh
-cd $TMPDIR
-git clone https://github.com/igaret/termux_stuff
-cp -r $TMPDIR/termux_stuff/termux_rish/* $PREFIX/
-chmod a+x $PREFIX/bin/*
-cd ~
-rm -rf $TMPDIR/termux_stuff
